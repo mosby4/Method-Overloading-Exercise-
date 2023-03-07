94f7e1988e6e3896a14b6d4058d4b88490df759a
Mosby4 <rufus.hamby@gmial.com>
2023-03-07 11:38:09 -0600

    first attempt

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,6 +2,34 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool istrue)
+        {
+            var sum = 0;
+            var response = "";
+            if (istrue)
+            {
+                sum = num1 + num2;
+                if (sum == 1)
+                {
+                    response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+            }
+            return response;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
